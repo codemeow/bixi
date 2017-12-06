1838b356dd816d150642c6940375f05b7b7664cb
codemeow <alexcones@gmail.com>
2017-12-06 22:20:41 +0300

    More tests

--- code/test/serials/tst_bxiserwrite.c
+++ code/test/serials/tst_bxiserwrite.c
@@ -24,100 +24,189 @@
 #include "../test.h"
 #include "../serials/tst_bxiserwrite.h"
 
-/* @todo good tests */
+static const u8 data[4] = { 0xf0, 0xf1, 0xf2, 0xf3 };
+
+static void test_serials_bxi_write_i8(void)
+{
+    u8 test[4] = { 0 };
+
+    printf("        checking: bxi_write_i8\n");
+
+    bxi_write_i8(-16, test);
+    if (bxi_memcmp(data, test, sizeof(i8)))
+        test_failed();
+}
+
+static void test_serials_bxi_write_u8(void)
+{
+    u8 test[4] = { 0 };
+
+    printf("        checking: bxi_write_u8\n");
+
+    bxi_write_u8(0xf0u, test);
+    if (bxi_memcmp(data, test, sizeof(u8)))
+        test_failed();
+}
+
+static void test_serials_bxi_write_i16(void)
+{
+    u8 test[4] = { 0 };
+
+    printf("        checking: bxi_write_i16\n");
+
+    bxi_write_i16(-3600, test);
+    if (bxi_memcmp(data, test, sizeof(i16)))
+        test_failed();
+}
+
+static void test_serials_bxi_write_u16(void)
+{
+    u8 test[4] = { 0 };
+
+    printf("        checking: bxi_write_u16\n");
+
+    bxi_write_u16(61936, test);
+    if (bxi_memcmp(data, test, sizeof(u16)))
+        test_failed();
+}
+
+static void test_serials_bxi_write_i32(void)
+{
+    u8 test[4] = { 0 };
+
+    printf("        checking: bxi_write_i32\n");
+
+    bxi_write_i32(-202182160, test);
+    if (bxi_memcmp(data, test, sizeof(i32)))
+        test_failed();
+}
+
+static void test_serials_bxi_write_u32(void)
+{
+    u8 test[4] = { 0 };
+
+    printf("        checking: bxi_write_u32\n");
+
+    bxi_write_u32(0xf3f2f1f0u, test);
+    if (bxi_memcmp(data, test, sizeof(u32)))
+        test_failed();
+}
+
+static void test_serials_bxi_write_i16_be(void)
+{
+    u8 test[4] = { 0 };
+
+    printf("        checking: bxi_write_i16_be\n");
+
+    bxi_write_i16_be(-3855, test);
+    if (bxi_memcmp(data, test, sizeof(i16_be)))
+        test_failed();
+}
+
+static void test_serials_bxi_write_u16_be(void)
+{
+    u8 test[4] = { 0 };
+
+    printf("        checking: bxi_write_u16_be\n");
+
+    bxi_write_u16_be(0xf0f1u, test);
+    if (bxi_memcmp(data, test, sizeof(u16_be)))
+        test_failed();
+}
+
+static void test_serials_bxi_write_i32_be(void)
+{
+    u8 test[4] = { 0 };
+
+    printf("        checking: bxi_write_i32_be\n");
+
+    bxi_write_i32_be(-252579085, test);
+    if (bxi_memcmp(data, test, sizeof(i32_be)))
+        test_failed();
+}
+
+static void test_serials_bxi_write_u32_be(void)
+{
+    u8 test[4] = { 0 };
+
+    printf("        checking: bxi_write_u32_be\n");
+
+    bxi_write_u32_be(0xf0f1f2f3u, test);
+    if (bxi_memcmp(data, test, sizeof(u32_be)))
+        test_failed();
+}
+
+static void test_serials_bxi_write_i16_le(void)
+{
+    u8 test[4] = { 0 };
+
+    printf("        checking: bxi_write_i16_le\n");
+
+    bxi_write_i16_le(-3600, test);
+    if (bxi_memcmp(data, test, sizeof(i16_le)))
+        test_failed();
+}
+
+static void test_serials_bxi_write_u16_le(void)
+{
+    u8 test[4] = { 0 };
+
+    printf("        checking: bxi_write_u16_le\n");
+
+    bxi_write_u16_le(61936, test);
+    if (bxi_memcmp(data, test, sizeof(u16_le)))
+        test_failed();
+}
+
+static void test_serials_bxi_write_i32_le(void)
+{
+    u8 test[4] = { 0 };
+
+    printf("        checking: bxi_write_i32_le\n");
+
+    bxi_write_i32_le(-202182160, test);
+    if (bxi_memcmp(data, test, sizeof(i32_le)))
+        test_failed();
+}
+
+static void test_serials_bxi_write_u32_le(void)
+{
+    u8 test[4] = { 0 };
+
+    printf("        checking: bxi_write_u32_le\n");
+
+    bxi_write_u32_le(0xf3f2f1f0u, test);
+    if (bxi_memcmp(data, test, sizeof(u32_le)))
+        test_failed();
+}
+
+static void test_serials_functions(void)
+{
+    printf("    functions:\n");
+
+    test_serials_bxi_write_i8();
+    test_serials_bxi_write_u8();
+    test_serials_bxi_write_i16();
+    test_serials_bxi_write_u16();
+    test_serials_bxi_write_i32();
+    test_serials_bxi_write_u32();
+
+    test_serials_bxi_write_i16_be();
+    test_serials_bxi_write_u16_be();
+    test_serials_bxi_write_i32_be();
+    test_serials_bxi_write_u32_be();
+
+    test_serials_bxi_write_i16_le();
+    test_serials_bxi_write_u16_le();
+    test_serials_bxi_write_i32_le();
+    test_serials_bxi_write_u32_le();
+}
 
 void test_serials_bxiserwrite(void)
 {
-    u8 data[4] = { 0xf0, 0xf1, 0xf2, 0xf3 };
-    u8 test[4];
-
     print_info;
 
-    printf("    functions:\n");
-
-    /* bxi_write_i8 */
-    printf("        checking: bxi_write_i8\n");
-    bxi_write_i8(-16, test);
-    if (bxi_memcmp(data, test, sizeof(i8)))
-        print_failed();
-
-    /* bxi_write_u8 */
-    printf("        checking: bxi_write_u8\n");
-    bxi_write_u8(0xf0u, test);
-    if (bxi_memcmp(data, test, sizeof(u8)))
-        print_failed();
-
-    /* bxi_write_i16 */
-    printf("        checking: bxi_write_i16\n");
-    bxi_write_i16(-3600, test);
-    if (bxi_memcmp(data, test, sizeof(i16)))
-        print_failed();
-
-    /* bxi_write_u16 */
-    printf("        checking: bxi_write_u16\n");
-    bxi_write_u16(61936, test);
-    if (bxi_memcmp(data, test, sizeof(u16)))
-        print_failed();
-
-    /* bxi_write_i32 */
-    printf("        checking: bxi_write_i32\n");
-    bxi_write_i32(-202182160, test);
-    if (bxi_memcmp(data, test, sizeof(i32)))
-        print_failed();
-
-    /* bxi_write_u32 */
-    printf("        checking: bxi_write_u32\n");
-    bxi_write_u32(0xf3f2f1f0u, test);
-    if (bxi_memcmp(data, test, sizeof(u32)))
-        print_failed();
-
-    /* bxi_write_i16_be */
-    printf("        checking: bxi_write_i16_be\n");
-    bxi_write_i16_be(-3855, test);
-    if (bxi_memcmp(data, test, sizeof(i16_be)))
-        print_failed();
-
-    /* bxi_write_u16_be */
-    printf("        checking: bxi_write_u16_be\n");
-    bxi_write_u16_be(0xf0f1u, test);
-    if (bxi_memcmp(data, test, sizeof(u16_be)))
-        print_failed();
-
-    /* bxi_write_i32_be */
-    printf("        checking: bxi_write_i32_be\n");
-    bxi_write_i32_be(-252579085, test);
-    if (bxi_memcmp(data, test, sizeof(i32_be)))
-        print_failed();
-
-    /* bxi_write_u32_be */
-    printf("        checking: bxi_write_u32_be\n");
-    bxi_write_u32_be(0xf0f1f2f3u, test);
-    if (bxi_memcmp(data, test, sizeof(u32_be)))
-        print_failed();
-
-    /* bxi_write_i16 */
-    printf("        checking: bxi_write_i16_le\n");
-    bxi_write_i16_le(-3600, test);
-    if (bxi_memcmp(data, test, sizeof(i16_le)))
-        print_failed();
-
-    /* bxi_write_u16_le */
-    printf("        checking: bxi_write_u16_le\n");
-    bxi_write_u16_le(61936, test);
-    if (bxi_memcmp(data, test, sizeof(u16_le)))
-        print_failed();
-
-    /* bxi_write_i32_le */
-    printf("        checking: bxi_write_i32_le\n");
-    bxi_write_i32_le(-202182160, test);
-    if (bxi_memcmp(data, test, sizeof(i32_le)))
-        print_failed();
-
-    /* bxi_write_u32_le */
-    printf("        checking: bxi_write_u32_le\n");
-    bxi_write_u32_le(0xf3f2f1f0u, test);
-    if (bxi_memcmp(data, test, sizeof(u32_le)))
-        print_failed();
+    test_serials_functions();
 
     print_passed();
 }
