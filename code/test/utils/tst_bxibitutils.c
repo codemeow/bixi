af6e13182e8317d1324822896e35d096b7eba375
codemeow <alexcones@gmail.com>
2017-12-17 22:12:50 +0300

    More tests

--- code/test/utils/tst_bxibitutils.c
+++ code/test/utils/tst_bxibitutils.c
@@ -24,80 +24,145 @@
 #include "../test.h"
 #include "../utils/tst_bxibitutils.h"
 
-/* @todo good tests */
+static void test_utils_test_bxi_swap(void)
+{
+    i32 a = 3;
+    i32 b = 4;
+
+#   ifndef BXI_SWAP
+#       define BXI_SWAP(x, y)
+#   endif
+
+    BXI_SWAP(a, b);
+    if ((a != 4) || (b != 3))
+        test_failed();
+
+    BXI_SWAP(a, a);
+    if ((a != 4))
+        test_failed();
+}
+
+static void test_utils_definitions(void)
+{
+    printf("    defines:\n");
+
+    TEST_BXI_MACRO_EXISTS_2(BXI_SWAP);
+        test_utils_test_bxi_swap();
+}
+
+static void test_utils_test_bxi_rotl8(void)
+{
+    printf("        checking: bxi_rotl8\n");
+
+    if (bxi_rotl8(0x4c, 5) != 0x89)
+        test_failed();
+}
+
+static void test_utils_test_bxi_rotr8(void)
+{
+    printf("        checking: bxi_rotr8\n");
+
+    if (bxi_rotr8(0x89, 5) != 0x4c)
+        test_failed();
+}
+
+static void test_utils_test_bxi_rotl16(void)
+{
+    printf("        checking: bxi_rotl16\n");
+
+    if (bxi_rotl16(0x1122, 4) != 0x1221)
+        test_failed();
+}
+
+static void test_utils_test_bxi_rotr16(void)
+{
+    printf("        checking: bxi_rotr16\n");
+
+    if (bxi_rotr16(0x1221, 4) != 0x1122)
+        test_failed();
+}
+
+static void test_utils_test_bxi_rotl32(void)
+{
+    printf("        checking: bxi_rotl32\n");
+
+    if (bxi_rotl32(0x12345678, 8) != 0x34567812)
+        test_failed();
+}
+
+static void test_utils_test_bxi_rotr32(void)
+{
+    printf("        checking: bxi_rotr32\n");
+
+    if (bxi_rotr32(0x34567812, 8) != 0x12345678)
+        test_failed();
+}
+
+static void test_utils_test_bxi_getbit(void)
+{
+    printf("        checking: bxi_getbit\n");
+
+    if (bxi_getbit(0x12345678, 9) != 1)
+        test_failed();
+}
+
+static void test_utils_test_bxi_setbit(void)
+{
+    printf("        checking: bxi_setbit\n");
+
+    if (bxi_setbit(0x12345678, 1) != 0x1234567A)
+        test_failed();
+}
+
+static void test_utils_test_bxi_tglbit(void)
+{
+    printf("        checking: bxi_tglbit\n");
+
+    if (bxi_tglbit(0x12345678, 9) != 0x12345478)
+        test_failed();
+}
+
+static void test_utils_test_bxi_clrbit(void)
+{
+    printf("        checking: bxi_clrbit\n");
+
+    if (bxi_clrbit(0x12345678, 9) != 0x12345478)
+        test_failed();
+}
+
+static void test_utils_test_bxi_cntbit(void)
+{
+    printf("        checking: bxi_cntbit\n");
+
+    if (bxi_cntbit(0x80000000u) != 1)
+        test_failed();
+    if (bxi_cntbit(0x89100) != 4)
+        test_failed();
+}
+
+static void test_utils_functions(void)
+{
+    printf("    functions:\n");
+
+    test_utils_test_bxi_rotl8();
+    test_utils_test_bxi_rotr8();
+    test_utils_test_bxi_rotl16();
+    test_utils_test_bxi_rotr16();
+    test_utils_test_bxi_rotl32();
+    test_utils_test_bxi_rotr32();
+    test_utils_test_bxi_getbit();
+    test_utils_test_bxi_setbit();
+    test_utils_test_bxi_tglbit();
+    test_utils_test_bxi_clrbit();
+    test_utils_test_bxi_cntbit();
+}
 
 void test_utils_bxibitutils(void)
 {
     print_info;
 
-    printf("    defines:\n");
-#   if defined(BXI_SWAP)
-        printf("        checking: BXI_SWAP\n");
-        {
-            i32 a = 3;
-            i32 b = 4;
-            BXI_SWAP(a, b);
-            if ((a != 4) || (b != 3))
-                print_failed();
-            BXI_SWAP(a, a);
-            if ((a != 4))
-                print_failed();
-        }
-#   else
-        print_failed();
-#   endif
-
-    printf("    functions:\n");
-
-    printf("        checking: bxi_rotl8\n");
-    if (bxi_rotl8(0x4c, 5) != 0x89)
-        print_failed();
-
-    printf("        checking: bxi_rotr8\n");
-    if (bxi_rotr8(0x89, 5) != 0x4c)
-        print_failed();
-
-    printf("        checking: bxi_rotl16\n");
-    if (bxi_rotl16(0x1122, 4) != 0x1221)
-        print_failed();
-
-    printf("        checking: bxi_rotl16\n");
-    if (bxi_rotr16(0x1221, 4) != 0x1122)
-        print_failed();
-
-    printf("        checking: bxi_rotl32\n");
-    if (bxi_rotl32(0x12345678, 8) != 0x34567812)
-        print_failed();
-
-    printf("        checking: bxi_rotl32\n");
-    if (bxi_rotr32(0x34567812, 8) != 0x12345678)
-        print_failed();
-
-    printf("        checking: bxi_getbit\n");
-    if (bxi_getbit(0x12345678, 9) != 1)
-        print_failed();
-
-    printf("        checking: bxi_setbit\n");
-    if (bxi_setbit(0x12345678, 1) != 0x1234567A)
-        print_failed();
-
-    printf("        checking: bxi_tglbit\n");
-    if (bxi_tglbit(0x12345678, 9) != 0x12345478)
-        print_failed();
-
-    printf("        checking: bxi_clrbit\n");
-    if (bxi_clrbit(0x12345678, 9) != 0x12345478)
-        print_failed();
-
-    printf("        checking: bxi_chgbit\n");
-    if (bxi_chgbit(0x12345678, 9, 0) != 0x12345478)
-        print_failed();
-
-    printf("        checking: bxi_cntbit\n");
-    if (bxi_cntbit(0x80000000u) != 1)
-        print_failed();
-    if (bxi_cntbit(0x89100) != 4)
-        print_failed();
+    test_utils_definitions();
+    test_utils_functions();
 
     print_passed();
 }
